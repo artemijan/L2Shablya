ec3db1ce2939b7c88a6c787bd7cb7b903770b2a5
Artem Fedorov <artem@feezback.com>
2024-11-07 21:49:11 +0100

    Small adjustments

---
+++ src/login_server/controller/gs_management.rs
@@ -0,0 +1,76 @@
+use std::net::Ipv4Addr;
+use tokio::sync::mpsc::Sender;
+use crate::common::dto::game_server::GSInfo;
+use crate::common::message::Request;
+use crate::packet::common::{ServerData, ServerStatus};
+use crate::packet::{error, GSLoginFailReasons};
+use crate::packet::login_fail::GSLogin;
+use super::data::Login;
+
+impl Login {
+    pub fn get_server_list(&self, client_ip: Ipv4Addr) -> Vec<ServerData> {
+        let mut servers = Vec::new();
+        for s in self.game_servers.iter() {
+            servers.push(ServerData {
+                ip: s.get_host_ip(client_ip),
+                port: i32::from(s.get_port()),
+                age_limit: i32::from(s.get_age_limit()),
+                pvp: s.is_pvp(),
+                current_players: 0,               //todo: implement me
+                max_players: s.get_max_players(), //allow wrapping
+                brackets: s.show_brackets(),
+                clock: false, //todo: implement me
+                status: ServerStatus::try_from(s.get_status()).ok(),
+                server_id: i32::from(s.get_id()),
+                server_type: s.get_server_type(),
+            });
+        }
+        servers
+    }
+
+    pub fn with_gs<F>(&self, gs_id: &u8, f: F) -> bool
+    where
+        F: Fn(&mut GSInfo),
+    {
+        if let Some(mut gs) = self.game_servers.get_mut(gs_id) {
+            f(&mut gs);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn register_gs(&self, gs_info: GSInfo) -> anyhow::Result<(), error::PacketRun> {
+        if let Some(allowed_gs) = &self.config.allowed_gs {
+            if !allowed_gs.contains_key(&gs_info.hex()) {
+                return Err(error::PacketRun {
+                    msg: Some(format!("GS wrong hex: {:}", gs_info.hex())),
+                    response: Some(Box::new(GSLogin::new(GSLoginFailReasons::WrongHexId))),
+                });
+            }
+        }
+        if !self.game_servers.contains_key(&gs_info.get_id()) {
+            self.game_servers.insert(gs_info.get_id(), gs_info);
+            Ok(())
+        } else {
+            Err(error::PacketRun {
+                msg: Some(format!(
+                    "GS already registered with id: {:}",
+                    gs_info.get_id()
+                )),
+                response: Some(Box::new(GSLogin::new(
+                    GSLoginFailReasons::AlreadyRegistered,
+                ))),
+            })
+        }
+    }
+
+
+    pub async fn remove_gs(&self, server_id: u8) {
+        self.game_servers.remove(&server_id);
+    }
+
+    pub fn connect_gs(&self, server_id: u8, gs_channel: Sender<(u8, Request)>) {
+        self.gs_channels.entry(server_id).or_insert(gs_channel);
+    }
+}
