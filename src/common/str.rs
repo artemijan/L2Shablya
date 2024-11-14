28979ad707332241d4023b967dabcb26ffd14ad0
Artem Fedorov <lineartem@gmail.com>
2024-11-14 20:17:15 +0100

    Fix docs

--- src/common/str.rs
+++ src/common/str.rs
@@ -9,11 +9,12 @@ pub trait Trim {
 impl StringTrim for String {
     ///this function is needed to trim also null bytes from a String
     /// # Example
+    /// 
     /// ```
-    /// let s = " ggg \0\0\0\".to_string();
+    /// let s = " ggg \0\0\0".to_string();
     /// s.trim_all()
-    /// _____________
-    /// $ ggg
+    /// ___________
+    /// ggg
     /// ```
     fn trim_all(self) -> String {
         String::from(self.trim_matches(|c: char| c.is_ascii_whitespace() || c == '\0'))
@@ -23,11 +24,12 @@ impl StringTrim for String {
 impl Trim for str {
     ///this function is needed to trim also null bytes from a str slice
     /// # Example
+    /// 
     /// ```
-    /// let s:&str = " ggg \0\0\0\";
+    /// let s:&'static str = " ggg \0\0\0";
     /// s.trim_all()
-    /// _____________
-    /// $ ggg
+    /// ___________
+    /// ggg
     /// ```
     fn trim_all(&self) -> &str {
         self.trim_matches(|c: char| c.is_ascii_whitespace() || c == '\0')
