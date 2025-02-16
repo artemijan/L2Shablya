ed78ac5b3ebb151e7b13a2ea090fe72e034a1482
Artem Fedorov <lineartem@gmail.com>
2025-02-16 19:46:27 +0100

    more unit tests

--- login/src/packet/from_client/req_auth_login.rs
+++ login/src/packet/from_client/req_auth_login.rs
@@ -55,6 +55,12 @@ impl HandleablePacket for RequestAuthLogin {
                 ban_ip: ActiveValue::NotSet,
             };
             user_record.save(pool).await?;
+        } else {
+            ch.send_packet(Box::new(PlayerLoginFail::new(
+                PlayerLoginFailReasons::ReasonUserOrPassWrong,
+            )?))
+            .await?;
+            bail!("User not found, and auto creation of accounts is disabled.");
         }
 
         ch.account_name = Some(self.username.to_string());
@@ -110,8 +116,20 @@ pub fn read_bytes(data: &[u8]) -> (String, String) {
 
 #[cfg(test)]
 mod tests {
+    use crate::client_thread::ClientHandler;
+    use crate::controller::LoginController;
     use crate::packet::from_client::RequestAuthLogin;
+    use crate::packet::HandleablePacket;
+    use entities::entities::user;
+    use entities::test_factories::factories::user_factory;
+    use l2_core::config::login::LoginServer;
     use l2_core::shared_packets::common::ReadablePacket;
+    use l2_core::traits::handlers::PacketHandler;
+    use l2_core::traits::ServerConfig;
+    use std::net::Ipv4Addr;
+    use std::sync::Arc;
+    use test_utils::utils::get_test_db;
+    use tokio::io::split;
 
     #[tokio::test]
     async fn test_read_bytes_login() {
@@ -130,5 +148,76 @@ mod tests {
         let p1 = RequestAuthLogin::read(&login_bytes).unwrap();
         assert_eq!(p1.username, "admin");
         assert_eq!(p1.password, "admin");
+    }
+    #[tokio::test]
+    async fn test_handle_login() {
+        let packet = RequestAuthLogin {
+            username: "admin".to_string(),
+            password: "admin".to_string(),
+        };
+        let db_pool = get_test_db().await;
+        let user_model = user_factory(&db_pool, |mut u| {
+            u.username = "admin".to_owned();
+            u.password = "$argon2id$v=19$m=19456,t=2,p=1$OnSjOZTt6Or9MxtqrcrGhw$GAY7oGKMMAQbd6tvWB96IjA6yxvZy2PMD2MEpHbmWS0".to_owned();
+            u
+        })
+        .await;
+        let (_client, server) = tokio::io::duplex(1024);
+        let cfg = LoginServer::from_string(include_str!("../../../../config/login.yaml"));
+        let lc = Arc::new(LoginController::new(Arc::new(cfg)));
+        let cloned_lc = lc.clone();
+        let ip = Ipv4Addr::new(127, 0, 0, 1);
+        let (r, w) = split(server);
+        let mut ch = ClientHandler::new(r, w, ip, db_pool, cloned_lc);
+        let res = packet.handle(&mut ch).await;
+        assert!(res.is_ok());
+        assert_eq!(ch.account_name, Some("admin".to_string()));
+    }
+    #[tokio::test]
+    async fn test_handle_login_auto_create_account() {
+        let packet = RequestAuthLogin {
+            username: "test".to_string(),
+            password: "test".to_string(),
+        };
+        let db_pool = get_test_db().await;
+        let (_client, server) = tokio::io::duplex(1024);
+        let mut cfg = LoginServer::from_string(include_str!("../../../../config/login.yaml"));
+        cfg.client.auto_create_accounts = true;
+        let lc = Arc::new(LoginController::new(Arc::new(cfg)));
+        let cloned_lc = lc.clone();
+        let ip = Ipv4Addr::new(127, 0, 0, 1);
+        let (r, w) = split(server);
+        let mut ch = ClientHandler::new(r, w, ip, db_pool.clone(), cloned_lc);
+        let res = packet.handle(&mut ch).await;
+        let user = user::Model::find_some_by_username(&db_pool, "test")
+            .await
+            .unwrap();
+        assert!(res.is_ok());
+        assert_eq!(ch.account_name, Some("test".to_string()));
+        assert!(user.is_some());
+        assert_eq!(user.unwrap().username, "test");
+    }
+
+    #[tokio::test]
+    async fn test_handle_login_auto_create_account_is_disabled() {
+        let packet = RequestAuthLogin {
+            username: "test".to_string(),
+            password: "test".to_string(),
+        };
+        let db_pool = get_test_db().await;
+        let (_client, server) = tokio::io::duplex(1024);
+        let mut cfg = LoginServer::from_string(include_str!("../../../../config/login.yaml"));
+        cfg.client.auto_create_accounts = false;
+        let lc = Arc::new(LoginController::new(Arc::new(cfg)));
+        let cloned_lc = lc.clone();
+        let ip = Ipv4Addr::new(127, 0, 0, 1);
+        let (r, w) = split(server);
+        let mut ch = ClientHandler::new(r, w, ip, db_pool.clone(), cloned_lc);
+        let res = packet.handle(&mut ch).await;
+        let user = user::Model::find_some_by_username(&db_pool, "test")
+            .await
+            .unwrap();
+        assert!(res.is_err());
+        assert!(user.is_none());
     }
 }
