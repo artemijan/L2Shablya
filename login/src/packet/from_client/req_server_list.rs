e3409f425f359393e8bc2e4b0013a42c0c0bfaa6
Artem Fedorov <lineartem@gmail.com>
2025-02-16 20:46:25 +0100

    remove unused code

--- login/src/packet/from_client/req_server_list.rs
+++ login/src/packet/from_client/req_server_list.rs
@@ -49,13 +49,14 @@ impl HandleablePacket for RequestServerList {
 mod tests {
     use super::*;
     use crate::controller::LoginController;
+    use crate::dto::game_server::GSInfo;
     use l2_core::config::login::LoginServer;
+    use l2_core::traits::handlers::PacketHandler;
     use l2_core::traits::ServerConfig;
     use std::net::Ipv4Addr;
     use std::sync::Arc;
     use test_utils::utils::get_test_db;
     use tokio::io::split;
-    use l2_core::traits::handlers::PacketHandler;
 
     #[test]
     fn test_read() {
@@ -75,6 +76,28 @@ mod tests {
         let mut cfg = LoginServer::from_string(include_str!("../../../../config/login.yaml"));
         cfg.client.auto_create_accounts = false;
         let lc = Arc::new(LoginController::new(Arc::new(cfg)));
+        lc.register_gs(
+            GSInfo::new(
+                1,
+                true,
+                false,
+                9106,
+                true,
+                1,
+                false,
+                1,
+                0,
+                false,
+                5000,
+                i128::from_str_radix("-2ad66b3f483c22be097019f55c8abdf0", 16)
+                    .unwrap()
+                    .to_be_bytes()
+                    .to_vec(),
+                &["192.168.0.100/8".to_string(), "192.168.0.0".to_string()],
+            )
+            .unwrap(),
+        )
+        .unwrap();
         let cloned_lc = lc.clone();
         let ip = Ipv4Addr::new(127, 0, 0, 1);
         let (r, w) = split(server);
