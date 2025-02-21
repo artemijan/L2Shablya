4a7de7a3e19b71a814d5f6ddb1188004fb49ef98
Artem Fedorov <lineartem@gmail.com>
2025-02-21 20:48:26 +0100

    More unit tests

--- game/src/packets/from_client/char_create.rs
+++ game/src/packets/from_client/char_create.rs
@@ -242,7 +242,6 @@ mod tests {
         assert!(res.is_ok());
         assert_eq!(ch.get_account_chars().unwrap().len(), 1);
         let res = pack.handle(&mut ch).await;
-        println!("{:?}", res.is_ok()); // there shouldn't be error
         assert_eq!(ch.get_account_chars().unwrap().len(), 1); // but char not created because it's duplicate
         client.shutdown().await.unwrap();
     }
