02a057e51d0149d2a3dcbd1bff75f064e29021a0
Artem Fedorov <lineartem@gmail.com>
2025-02-21 21:18:27 +0100

    More unit tests

--- game/src/packets/from_client/char_select.rs
+++ game/src/packets/from_client/char_select.rs
@@ -35,6 +35,9 @@ impl HandleablePacket for SelectChar {
         if handler.get_selected_char_slot().is_some() {
             bail!("Char already selected")
         }
+        handler.try_get_session_key()?;
+        handler.try_get_user()?;
+        handler.try_get_char_by_slot_id(self.char_slot)?;
         let game_time = handler.get_controller().get_game_time();
         handler.set_status(ClientStatus::Entering);
         handler.select_char(self.char_slot);
@@ -47,5 +50,57 @@ impl HandleablePacket for SelectChar {
             )?))
             .await?;
         Ok(())
+    }
+}
+#[cfg(test)]
+mod tests {
+    use std::net::Ipv4Addr;
+    use std::sync::Arc;
+    use ntest::timeout;
+    use tokio::io::{split, AsyncWriteExt};
+    use entities::dao::char_info::CharacterInfo;
+    use entities::test_factories::factories::{char_factory, user_factory};
+    use l2_core::config::gs::GSServer;
+    use l2_core::session::SessionKey;
+    use l2_core::traits::ServerConfig;
+    use test_utils::utils::get_test_db;
+    use crate::controller::Controller;
+    use super::*;
+    #[tokio::test]
+    #[timeout(3000)]
+    async fn test_handle() {
+        let pool = get_test_db().await;
+        let pack = SelectChar { char_slot: 0 };
+        let (mut client, server) = tokio::io::duplex(1024);
+        let (r, w) = split(server);
+        let cfg = Arc::new(GSServer::from_string(include_str!(
+            "../../../../test_data/game.yaml"
+        )));
+        let controller = Arc::new(Controller::new(cfg));
+        controller.add_online_account(String::from("test"));
+        let user = user_factory(&pool, |mut u| {
+            u.username = String::from("test");
+            u
+        })
+            .await;
+        let char_model = char_factory(&pool, |mut u| {
+            u.user_id = user.id;
+            u.name = "Char".to_owned();
+            u
+        })
+            .await;
+        let mut ch = ClientHandler::new(r, w, Ipv4Addr::LOCALHOST, pool.clone(), controller);
+        let res = pack.handle(&mut ch).await;
+        assert!(res.is_err());
+        ch.set_session_key(SessionKey::new());
+        let res = pack.handle(&mut ch).await;
+        assert!(res.is_err());
+        ch.set_user(user);
+        let res = pack.handle(&mut ch).await;
+        assert!(res.is_err());
+        ch.set_account_chars(vec![CharacterInfo::new(char_model, vec![]).unwrap()]);
+        let res = pack.handle(&mut ch).await;
+        assert!(res.is_ok());
+        client.shutdown().await.unwrap();
     }
 }
